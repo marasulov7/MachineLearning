114767817e4ea3c0c0705647b27338183f693849
marasulov7 <marasulovalish@gmail.com>
2025-01-29 16:15:23 +0500

    Добавление тренировочного набора данных

---
+++ SimpleLearning.py
@@ -0,0 +1,21 @@
+#  y = 7*x1 + 5*x2 + 3*x3
+
+from random import randint
+from sklearn.linear_model import LinearRegression
+
+# Создание тренировочного набора данных
+train_set_limit = 1000
+train_set_count = 100
+
+train_input = list()
+train_output = list()
+for i in range(train_set_count):
+    a = randint(0, train_set_limit)
+    b = randint(0, train_set_limit)
+    c = randint(0, train_set_limit)
+    op = (7*a)+(5*b)+(3*c)
+    train_input.append([a, b, c])
+    train_output.append([op])
+
+for i in range(20):
+    print(train_input[i], train_output[i])
